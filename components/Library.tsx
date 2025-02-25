0a188914fda073a1497502992d22bc913fe774eb
Shalih Al-Mujaddid <rowrrhehe@gmail.com>
2025-02-25 08:06:23 +0800

    first commit

--- components/Library.tsx
+++ components/Library.tsx
@@ -48,7 +48,16 @@ const Library = () => {
                      transition"
                     />
             </div>
-           
+            <div className="
+            flex
+            flex-col
+            gap-y-2
+            mt-4
+            px-3
+            ">
+                List of Songs!
+                
+         </div>           
          </div>
     );
     
